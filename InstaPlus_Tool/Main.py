0cc7ce469ab172241845db340a0897b8ded691bb
Namaste Hacker <95855028+NamasteAbhi@users.noreply.github.com>
2024-01-24 17:26:26 +0530

    Update Main.py

--- InstaPlus_Tool/Main.py
+++ InstaPlus_Tool/Main.py
@@ -2,7 +2,7 @@ from InstaPlus import InstaPlus
 
 
 UserName='ABCDEFGH' #Instagram UserName Here
-UserID='49078425082' #Enter Your Instagram UserID Here
+UserID='49079085082' #Enter Your Instagram UserID Here
 
 
 cl = InstaPlus(UserName,UserID)
