a79b58156d4687604a1fbaba9bce224b0de0433c
Namaste Hacker <95855028+NamasteAbhi@users.noreply.github.com>
2024-01-24 17:08:36 +0530

    Update InstaPlus.py

--- InstaPlus_Tool/InstaPlus.py
+++ InstaPlus_Tool/InstaPlus.py
@@ -158,43 +158,4 @@ class InstaPlus:
         except Exception as E:
             print(E)
 
-    # def CoinsTransfer(self):
-    #
-    #     key = 'mo1yDn01E3yVWM8lIKdMfpo92FN79uwq99p5Mm1I3CCcvx0FSKXu84YPJGlMwkKr'
-    #
-    #     Payload='{"prc":"0","cpn":"ir.neolook.followerpluss","cnt":"'+str(self.coins)+'","usi":"'+self.usi+'","apsk":"'+self.apsk+'","id":"49078425082","lang":"en","mrt":"Cafebazaar"}'
-    #
-    #
-    #     headers = {
-    #             # 'accept-encoding': 'gzip',
-    #             'connection': 'Close',
-    #             'content-transfer-encoding': 'binary',
-    #             'content-type': 'binary/octet-stream',
-    #             'host': 'app.neolook.ir',
-    #             "User-Agent": f"Dalvik/2.1.0 (Linux; U; Android {self.android}; {self.model} Build/PI)537.36",
-    #             'x-neolook-auth': base64.b64encode(f'neo_{self.neo}:{self.token}'.encode('utf-8')), #neo_18ec-4e74-4b:c13wnecYgBapttQwNjFxQwaS2aGYYx
-    #         }
-    #
-    #
-    #     response = self.Client.post(
-    #         'https://app.neolook.ir/insfl.10/follower/transferCoins/1092/ir.neolook.followerpluss',
-    #         headers=headers,
-    #         data=encrypt_payload(Payload, key),
-    #     )
-    #
-    #     print(decrypt_payload(response.content, key))
-
-
-# if __name__ == '__main__':
-#     def run():
-#         # print(len('vlAhbWFM9rkpSznOCBMfVxadtNaLkLbh'))
-#         cl=InstaPlus()
-#         cl.appStatus()
-#         cl.SignUp()
-#         cl.GetFollowList()
-#
-#
-#     for i in range(1):
-#         ml=threading.Thread(target=run)
-#         ml.start()
 
