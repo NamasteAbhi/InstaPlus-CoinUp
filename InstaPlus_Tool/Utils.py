eb7e24f35de757b0a7768ee5fed4eb8b1d039c07
Namaste Hacker <95855028+NamasteAbhi@users.noreply.github.com>
2024-01-24 17:08:57 +0530

    Update Utils.py

--- InstaPlus_Tool/Utils.py
+++ InstaPlus_Tool/Utils.py
@@ -156,16 +156,3 @@ def Gen_UserAgent():
     return random_key,random_value
 
 
-# if __name__ == '__main__':
-#     data='1d 33 3c 1a 6a 00 33 5c 32 06 0c 36 4e 35 3e 41 6c 4d 0f 4e 65 4d 45 51 2a 77 0c 2c 1f 3c 0f 51 7d 20 7f 60 34 69 71 7e 36 37 46 17 1d 64 65 1c 79 2c 77 3b 7e 41 23 56 31 65 2d 49 22 2d 36 0f 5c 04 4c 2f 77 2f 65 38 12 3e 6c 15 74 4b 02 14 51 22 56 0e 0f 23 70 03 50 36 44 00 63 15 3e 54 0c 29 3d 57 15 5e 14 0a 78 53 21 4a 45 29 03 53 09 73 31 4c 7c 0d 6a 3b 64 20 21 31 05 3b 58 31 ca 4d c4 5d e5 40 bc 45 9d 63 ff 4c 96 36 b0 1e b7 33 8a 33 ca 6e da 5b c5 7b f7 7e fd 2a f7 47 df 5b ce 6f c3 10 90 09 9f 55 e7 65 ff 12 88 73 9d 5a 8e 52 c7 29 de 37 cb 16 93 03 e1 40 e2 4a e1 64 d6 6b f1 52 fe 49 b7 56 93 70 8b 7e f2 58 a7 70 bc 77 cc 35 f8 2e db 06 f0 18 a0 0a ac 7f 98 04 9f 42 da 19 8a 53 d4 0f e3 30 ab 08 dc 77 d1 44 df 53 d1 35 c5 7e 80'.replace(' ','')
-#     print(data)
-#     key='mo1yDn01E3yVWM8lIKdMfpo92FN79uwq99p5Mm1I3CCcvx0FSKXu84YPJGlMwkKr'
-#     de=decrypt_payload(bytes.fromhex(data),key)
-#     print(de)
-#
-#
-#
-#     from NamasteAes import NamasteAes
-#
-#
-#     print(NamasteAes.dec_ecb('ZR+7SLTVUGUOGxNxpBYfe7h9Csw+D42wRTYn2tr64zfmZHggQScMLYRmwHpROe0ztYjVSVb7uJkWJJi0HcTQIOgf8yy9vPDJrnXqZdJG+No=', "ksd%lsld@@ls86ms@lmsf4f8",16))
